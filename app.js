caa48446bd96e27d4e7916621e7701f17dadb210
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-12-15 20:34:00 -0500

    feat: profiling server

--- app.js
+++ app.js
@@ -1,39 +1,78 @@
-import express from 'express'
-import cluster from 'cluster'
-import { cpus } from 'os'
-import isPrime from './utils.js'
+const express = require("express");
+const crypto = require("crypto");
 
-const PORT = parseInt(process.argv[2]) || 8080
-const modoCluster = process.argv[3] == 'CLUSTER'
+const app = express();
 
-if (modoCluster && cluster.isPrimary) {
-   const numCPUs = cpus().length
+const users = {}
 
-   console.log(`Número de procesadores: ${numCPUs}`)
-   console.log(`PID MASTER ${process.pid}`)
+app.use(express.static('public'))
 
-   for (let i = 0; i < numCPUs; i++) {
-       cluster.fork()
-   }
+const PORT = parseInt(process.argv[2]) || 8080;
+const server = app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
+server.on("error", (error) => console.log(`Error en servidor: ${error}`));
 
-   cluster.on('exit', worker => {
-       console.log('Worker', worker.process.pid, 'died', new Date().toLocaleString())
-       cluster.fork()
-   })
-} else {
-   const app = express()
+app.get("/getUsers", (req, res) => {
+    res.json({ users })
+  })
+  
+  app.get("/newUser", (req, res) => {
+    let username = req.query.username || "";
+    const password = req.query.password || "";
+  
+    username = username.replace(/[!@#$%^&*]/g, "");
+  
+    if (!username || !password || users[username]) {
+      return res.sendStatus(400);
+    }
+  
+    const salt = crypto.randomBytes(128).toString("base64");
+    const hash = crypto.pbkdf2Sync(password, salt, 10000, 512, "sha512");
+  
+    users[username] = { salt, hash };
+  
+    res.sendStatus(200);
+  });
+  
+  app.get("/auth-bloq", (req, res) => {
+    let username = req.query.username || "";
+    const password = req.query.password || "";
+  
+    username = username.replace(/[!@#$%^&*]/g, "");
+  
+    if (!username || !password || !users[username]) {
+      process.exit(1)
+      // return res.sendStatus(400);
+    }
+  
+    const { salt, hash } = users[username];
+    const encryptHash = crypto.pbkdf2Sync(password, salt, 10000, 512, "sha512");
+  
+    if (crypto.timingSafeEqual(hash, encryptHash)) {
+      res.sendStatus(200);
+    } else {
+      process.exit(1)
+      // res.sendStatus(401);
+    }
+  });
 
-   app.get('/', (req, res) => {
-       const primes = []
-       const max = Number(req.query.max) || 1000
-       for (let i = 1; i <= max; i++) {
-           if (isPrime(i)) primes.push(i)
-       }
-       res.json(primes)
-   })
-
-   app.listen(PORT, () => {
-       console.log(`Servidor express escuchando en el puerto ${PORT}`)
-       console.log(`PID WORKER ${process.pid}`)
-   })
-}
+  app.get("/auth-nobloq", (req, res) => {
+    let username = req.query.username || "";
+    const password = req.query.password || "";
+    username = username.replace(/[!@#$%^&*]/g, "");
+  
+    if (!username || !password || !users[username]) {
+      process.exit(1)
+      // return res.sendStatus(400);
+    }
+    crypto.pbkdf2(password, users[username].salt, 10000, 512, 'sha512', (err, hash) => {
+      if (users[username].hash.toString() === hash.toString()) {
+        res.sendStatus(200);
+      } else {
+        process.exit(1)
+        //res.sendStatus(401);
+      }
+    });
+  });
+  
